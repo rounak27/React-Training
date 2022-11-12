3eb2ff140c225228ba8551d746342f00cdf45f1a
Rounak <rounak.rajbhandari.com>
2022-11-12 09:23:55 +0545

    Deleted

--- my-app/src/component/square.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function Square(props){
- const [value,setValue]=useState(null);
-  
-    return( <button className="square" onClick={props.onClick}>
-    {props.value}
-  </button>);
-}
